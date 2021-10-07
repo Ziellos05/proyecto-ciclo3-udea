98270019b73cdb4e1a1da172554135667c1c0f83
Santiago Marulanda Meza <santiago.marulandam@gmail.com>
2021-10-06 21:11:43 -0500

    state management on sales screen

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,17 @@
 var express = require("express");
+var mongoose = require('mongoose');
 var app = express();
 
 const productsRoutes = require("./routes/products");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//TODO add string to connect to db
+mongoose.connect('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+    .then(() => {
+        console.log('Connected to DB!')
+    });
 
 app.use("/api/products", productsRoutes);
 
--- src/screens/register-sale/ProductosDisplay.js
+++ src/screens/register-sale/ProductosDisplay.js
@@ -9,16 +9,26 @@ import { listaProductos } from '../../utilities/listaProductos';
 
 const ProductosDisplay = () => {
 
+    const [newList, setNewList] = useState([]);
+
+    const [currentProduct, setCurrentProduct] = useState({});
+
     const [value, setValue] = useState("");
     const handleChange = (event) => {
       console.log(event.target.value);
       // DropdownButton.title = value;
       // console.log(value);
     }
+
+    const onPressProduct = (product) => { 
+      newList.push(product);
+      setCurrentProduct(product);
+      //alert(`${JSON.stringify(product)}`);
+    };
     
     return(
-      <DropdownButton id="dropdown-basic-button" title="Productos en inventario" className="mb-2">
-        {listaProductos.map((product) => (<Dropdown.Item key={product.id} value={product.name} onClick={handleChange}>{product.name}</Dropdown.Item>))}
+      <DropdownButton id="dropdown-basic-button" title={currentProduct.name || "Productos en inventario"} className="mb-2">
+        {listaProductos.map((product) => (<Dropdown.Item key={product.id} value={product.name} onClick={() => onPressProduct(product)}>{product.name}</Dropdown.Item>))}
       </DropdownButton>
     
     )
